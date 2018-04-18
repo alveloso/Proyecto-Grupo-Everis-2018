c6ca2bef898edb48f95fef838ebbf7d348bb8c34
alveloso <alveloso@usal.es>
2018-04-19 00:05:04 +0200

    Update PreguntasDAOImpl.java

--- zProyectoFinEverisGrupalV0002/src/daosImpl/PreguntasDAOImpl.java
+++ zProyectoFinEverisGrupalV0002/src/daosImpl/PreguntasDAOImpl.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import modelo.Preguntas;
+import modelos.Preguntas;
 import daos.ConstantesSQL;
 import daos.GenericDAO;
 import daos.PreguntasDAO;
