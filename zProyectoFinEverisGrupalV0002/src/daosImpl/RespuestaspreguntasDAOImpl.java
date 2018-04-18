eb019bb5dc6a86ac566694829190dd4ef21a4e7e
alveloso <alveloso@usal.es>
2018-04-19 00:05:40 +0200

    Update RespuestaspreguntasDAOImpl.java

--- zProyectoFinEverisGrupalV0002/src/daosImpl/RespuestaspreguntasDAOImpl.java
+++ zProyectoFinEverisGrupalV0002/src/daosImpl/RespuestaspreguntasDAOImpl.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 
-import modelo.Respuestaspreguntas;
+import modelos.Respuestaspreguntas;
 import daos.ConstantesSQL;
 import daos.GenericDAO;
 import daos.RespuestaspreguntasDAO;
